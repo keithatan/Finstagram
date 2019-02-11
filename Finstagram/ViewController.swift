9475bd981e3f02e413f6c573521fa830db0d8ae2
keithatan <tan230@purdue.edu>
2019-02-11 18:48:48 -0500

    Initial Commit

---
+++ Finstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finstagram
+//
+//  Created by Keith Tan on 2/11/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
