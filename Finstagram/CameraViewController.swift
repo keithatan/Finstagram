467d68d5eb16ae4789c7ac2924dc86321c887948
keithatan <tan230@purdue.edu>
2019-02-17 23:06:08 -0500

    Post photo to database functionality

--- Finstagram/CameraViewController.swift
+++ Finstagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -46,6 +47,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func submit(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = captionField.text
+        post["owner"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        
+        let file = PFFileObject(data:imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+            }
+            else {
+                
+            }
+        }
+        
         
     }
     /*
