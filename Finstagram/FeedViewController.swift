5ab55c527fa80325afd25b7fdba17070b35634bc
keithatan <tan230@purdue.edu>
2019-02-17 23:36:12 -0500

    Retrieve full post

--- Finstagram/FeedViewController.swift
+++ Finstagram/FeedViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("owner")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (response, error) in
+            if self.posts != nil{
+                self.posts = response!
+                self.tableView.reloadData()
+            }
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell
+        
+        let post = posts[indexPath.row]
+        let user = post["owner"] as! PFUser
+        cell.nameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoImageView.af_setImage(withURL: url)
+        
+        return cell
     }
     
 
